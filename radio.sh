62efa8adac5a21b7fb6419f7277e667cfc1aa8fe
Nuno Filipe Gomes Dos Santos <nunofilipesantos@gmail.com>
2015-07-22 12:04:15 +0100

    A few correction on radio file

--- radio.sh
+++ radio.sh
@@ -14,17 +14,6 @@ echo "Installing FM Radio"
 echo ""
 echo ""
 
-sudo apt-get install libsndfile1-dev
-git clone https://github.com/ChristopheJacquet/PiFmRds.git
-cd PiFmRds/src
-make
-sudo ./pi_fm_rds -audio stereo_44100.wav -ps UJUNIORFM
-
-echo ""
-echo "Running on FM 107.9MHz (JUNIORFM)"
-echo ""
-echo ""
-
 echo ""
 echo "If you want to change parameters use the following command:"
 echo "pi_fm_rds [-freq freq] [-audio file] [-ppm ppm_error] [-pi pi_code] [-ps ps_text] [-rt rt_text]"
@@ -34,7 +23,17 @@ echo "https://github.com/ChristopheJacquet/PiFmRds"
 echo "pi_fm_rds [-freq freq] [-audio file] [-ppm ppm_error] [-pi pi_code] [-ps ps_text] [-rt rt_text]"
 echo ""
 
-#To play audio from USB Mic uncomment the following line
+sudo apt-get install libsndfile1-dev
+git clone https://github.com/ChristopheJacquet/PiFmRds.git
+cd PiFmRds/src
+make
 
+echo ""
+echo "Running on FM 107.9MHz (JUNIORFM)"
+echo ""
+echo ""
 
+sudo ./pi_fm_rds -audio stereo_44100.wav -ps UJUNIORFM
+
+#To play audio from USB Mic uncomment the following line:
 #arecord -fS16_LE -r 22050 -Dplughw:1,0 - | sudo ./pifm - 107.9 22050
